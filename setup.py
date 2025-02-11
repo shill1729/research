1f4741d2c453e34ad1700146682cf795c76a1c1b
seanhill <seamhill93@gmail.com>
2025-02-11 13:58:46 -0500

    update initial freq in wave surface and version change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 
 setup(
     name='ae',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(include=["ae", "ae.*"]),
     exclude_package_data={"": ["experiments/*"]},
     url='https://github.com/shill1729/research',
