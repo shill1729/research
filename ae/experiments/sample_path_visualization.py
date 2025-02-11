9fd743c8d6e7aeab46628cce91c9a4c182319932
seanhill <seamhill93@gmail.com>
2025-02-11 13:27:40 -0500

    configure examples and edit requirements.txt

--- ae/experiments/sample_path_visualization.py
+++ ae/experiments/sample_path_visualization.py
@@ -22,12 +22,10 @@ if __name__ == "__main__":
 
     # Inputs
     device = torch.device("cpu")
-    train_seed = None
-    test_seed = None
+    train_seed = 17
+    test_seed = 42
     norm = "fro"
-    embed = False
-    R_seed = 123  # Set your seed for the random projection
-    # torch.manual_seed(train_seed)
+    torch.manual_seed(train_seed)
     # Point cloud parameters
     num_points = 30
     num_test = 100
@@ -40,13 +38,13 @@ if __name__ == "__main__":
     lr = 0.001
     weight_decay = 0.
     # EPOCHS FOR TRAINING
-    epochs_ae = 2000
-    epochs_diffusion = 2000
-    epochs_drift = 2000
+    epochs_ae = 5000
+    epochs_diffusion = 5000
+    epochs_drift = 5000
     batch_size = 15
-    print_freq = 100
+    print_freq = 500
     # PATHS PARAMETERS
-    ntime = 1000
+    ntime = 5000
     npaths = 5
     tn = 2.5
     # weights for different penalties
@@ -56,12 +54,6 @@ if __name__ == "__main__":
     weights.tangent_angle_weight = 0.
     weights.tangent_drift_weight = 0.
     weights.diffeomorphism_reg = 0.
-
-    # Generate a fixed random matrix R with a seed
-    torch.manual_seed(R_seed)
-
-    D = 100  # Desired embedding dimension
-    R = torch.randn(D, 3)  # Random matrix R of size (D, 3)
 
     # Activation functions
     encoder_act = nn.Tanh()
@@ -88,16 +80,6 @@ if __name__ == "__main__":
     x, _, mu, cov, local_x = cloud.generate(num_points, seed=train_seed)
     x, mu, cov, p, _, orthonormal_frame = process_data(x, mu, cov, d=2, return_frame=True, device=device)
 
-    # TODO: refactor this into a function in utils.
-    if embed:
-        print("Embedding into " + str(D) + " dimensions")
-        x = torch.matmul(x, R.T)
-        mu = torch.matmul(mu, R.T)
-        # Transform covariance for each sample in the batch
-        cov = torch.einsum('ij,bjk,kl->bil', R, cov, R.T)  # Result: (n, D, D)
-        x, mu, cov, p, _, orthonormal_frame = process_data(x, mu, cov, d=2, return_frame=True)
-        extrinsic_dim = D
-
     ae = AutoEncoder(extrinsic_dim, intrinsic_dim, hidden_dims, encoder_act, decoder_act)
     latent_sde = LatentNeuralSDE(intrinsic_dim, drift_layers, diffusion_layers, drift_act, diffusion_act, encoder_act)
     ae_diffusion = AutoEncoderDiffusion(latent_sde, ae)
@@ -109,7 +91,6 @@ if __name__ == "__main__":
     # Now all three components have been trained, we assess the performance on the test set.
     # returns points, weights, drifts, cov, local coord
     # Generate the point cloud plus dynamics observations
-    # TODO this will fail on embed=True until the above is taken care of regarding embedding
     cloud = PointCloud(manifold, large_bounds, local_drift, local_diffusion, compute_orthogonal_proj=True)
     x_test, _, mu_test, cov_test, local_x_test = cloud.generate(num_test, seed=test_seed)
     x_test, mu_test, cov_test, p_test, _, orthonormal_frame_test = process_data(x_test,
@@ -166,12 +147,7 @@ if __name__ == "__main__":
         model_ambient_paths[j, :, :] = ae.decoder(
             torch.tensor(model_latent_paths[j, :, :], dtype=torch.float32)).detach().numpy()
         for i in range(ntime + 1):
-            if embed:
-                path3 = np.squeeze(cloud.np_phi(*true_latent_paths[j, i, :]))
-                pathD = np.matmul(path3, R.T)
-                true_ambient_paths[j, i, :] = pathD
-            else:
-                true_ambient_paths[j, i, :] = np.squeeze(cloud.np_phi(*true_latent_paths[j, i, :]))
+            true_ambient_paths[j, i, :] = np.squeeze(cloud.np_phi(*true_latent_paths[j, i, :]))
 
     x_test = x_test.detach()
     fig = plt.figure()
